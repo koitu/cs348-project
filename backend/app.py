54b313be066e4fbf7ad5a32ebdb4e92d48666cd0
Andrew Wang <someone.zip@gmail.com>
2023-03-05 21:48:46 -0500

    move db into backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,35 @@
 from flask import Flask
 from flask_cors import CORS
+# from flaskext.mysql import MySQL
+# from flask_mysqldb import MySQL
+
+import click
 
 
 app = Flask(__name__)
 CORS(app)
+
+# app.config['MYSQL_DATABASE_HOST'] = "127.0.0.1"
+# app.config['MYSQL_DATABASE_USER'] = "root"
+# app.config['MYSQL_DATABASE_PORT'] = "5555"
+# app.config['MYSQL_DATABASE_PASSWORD'] = "password"
+# app.config['MYSQL_DATABASE_DB'] = 'cs348'
+# mysql = MySQL()
+
+# mysql = MySQL(app,
+#               prefix="database",
+#               host="localhost:5555",
+#               user="root",
+#               password="password",
+#               autocommit=True
+# )
+
+# mysql.init_app(app)
+
+# @click.command('init-db')
+# def init_db():
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
 
 
 if __name__ == "__main__":
--- backend/db/SampleData.sql
+++ backend/db/SampleData.sql
@@ -1,7 +1,7 @@
-INSERT INTO Player VALUES 
+INSERT INTO Player VALUES
 	(8478421, 'A.J. Greer', '1996-12-14', 210, 190, 'CAN', NULL, 24, 'L'),
     (8478424, 'Jansen Harkins', '1997-05-23', 182, 186, 'USA', NULL, 58, 'C'),
     (8478425, 'Brendan Guhle', '1997-07-29', 196, 183, 'CAN', NULL, 45, 'D'),
     (8478427, 'Sebastian Aho', '1997-07-26', 176, 1, 'FIN', NULL, 20, 'C'),
     (8478430, 'Oliver Kylington', '1997-05-19', 183, 180, 'SWE', NULL, 58, 'D');
-    
+
